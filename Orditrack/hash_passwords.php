b1a6642f200ea73deabef320b13eee4134026f56
JM97400 <martin.jacob@hotmail.fr>
2025-04-07 14:59:28 +0400

    Modification du hash_passwords.php

--- Orditrack/hash_passwords.php
+++ Orditrack/hash_passwords.php
@@ -1,20 +1,23 @@
 <?php
-/*
+
+////////////////////////////////////////////////////////////////////////////////////////////
+/////////// !!  Taper la commande dans le terminal : php hash_passwords.php !! ////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+/* Quand l'utilisateur existe déjà ==>
 $password = '1234'; // Mot de passe actuel de user et admin
 echo password_hash($password, PASSWORD_DEFAULT) . "\n";
 
+// Pour modifier un mot de passe ==>
 $new_password = 'nouveauMDP'; // Remplace par le mot de passe souhaité
 echo password_hash($new_password, PASSWORD_DEFAULT) . "\n";
 */
 
-// hash_passwords.php
+//////// Création d'un nouveau mot de passe pour nouvel utilisateur ////////hash_passwords.php
 $new_password = '1234'; // Mot de passe lisible choisi pour le nouvel utilisateur
 $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
 echo "Mot de passe lisible : $new_password\n";
 echo "Mot de passe haché : $hashed_password\n";
-
-
-
-
 
 ?>
